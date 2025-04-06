bf4e678977d20dddfd83b65e9331c86c32f3543d
유열 <vflag21c@nate.com>
2025-04-06 11:32:41 +0900

    람다가 필요한 이유

---
+++ src/lambda/lambda1/MyFunction2.java
@@ -0,0 +1,16 @@
+package lambda.lambda1;
+
+import lambda.MyFunction;
+
+public class MyFunction1 {
+    public static void main(String[] args) {
+        new MyFunction() {
+            @Override
+            public int apply(int a, int b) {
+                return a + b;
+            }
+        };
+
+        int result = 
+    }
+}
---
+++ src/lambda/lambda1/ProcedureMain1.java
@@ -0,0 +1,4 @@
+package lambda.lambda1;
+
+public class ProcedureMain1 {
+}
